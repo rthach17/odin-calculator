1964c523d0db44f7894268853bffbd3404501b0c
Ravy Thach <rthach17@gmail.com>
2022-12-24 23:22:33 -0800

    Add calculator logic and functionality

--- script/script.js
+++ script/script.js
@@ -1,12 +1,113 @@
 const inputWrapper = document.getElementById('input-wrapper');
 const inputs = document.querySelectorAll('button');
-const display = document.querySelector('p.display-input');
+const displayInput = document.querySelector('p.display-input');
+const displayOutput = document.querySelector('p.display-output');
+const expression = [];
+let operatorClicked = false;
+
+function updateDisplays(button) {
+  if (button === "AC") clearAll();
+  if (button === "DEL") {
+    if (operatorClicked) {
+      clearExpression();
+      operatorClicked = false;
+    }
+    removeCharFromDisplay(displayInput);
+  }
+
+  if (Number(button)) {
+    if (operatorClicked) {
+      displayInput.textContent = "";
+      operatorClicked = false;
+    }
+    addCharToDisplay(button, displayInput);
+  }
+  if ("+-*/".includes(button)) {
+    if (!isEmpty(displayInput.textContent) && isEmpty(expression)) {
+      expression.push(displayInput.textContent);
+      expression.push(button);
+      operatorClicked = true;
+    }
+    if (operatorClicked) expression[1] = button;
+  }
+  if (button === "=") {
+    if (expression.length === 2 && !isEmpty(displayInput.textContent)) {
+      expression.push(displayInput.textContent);
+      let result = operate(expression[0], expression[1], expression[2]);
+      displayOutput.textContent = result;
+      clearExpression();
+      clearDisplay(displayInput);
+      operatorClicked = false;
+    }
+  }
+
+  return;
+}
+
+function clearAll() {
+  clearDisplay(displayInput);
+  clearDisplay(displayOutput);
+  clearExpression();
+  operatorClicked = false;
+}
+
+function clearDisplay(displayElement) {
+  displayElement.textContent = "";
+}
+
+function clearExpression() {
+  expression.length = 0;
+}
+
+function removeCharFromDisplay(displayElement) {
+  let currentText = displayElement.textContent;
+  displayElement.textContent = currentText.slice(0, -1);
+}
+
+function addCharToDisplay(char, displayElement) {
+  displayElement.textContent += char; 
+}
+
+function operate(a, operator, b) {
+  if (operator === "+") return add(a, b);
+  if (operator === "-") return subtract(a, b);
+  if (operator === "*") return multiply(a, b);
+  if (operator === "/") return divide(a, b);
+  return;
+}
+
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  result = a * b;
+
+  return result;
+}
+
+function divide(a, b) {
+  result = a / b;
+  if (String(result).length > 8) return result.toPrecision(4);
+
+  return result;
+}
+
+function isEmpty(variable) {
+  if (variable.length === 0) return true;
+  return false;
+}
 
 // Button Input
 inputWrapper.addEventListener("click", event => {
-  if (event.target.nodeName == "BUTTON") {
-    display.textContent = event.target.textContent;
-    }
+  if (event.target.nodeName !== "BUTTON") return;
+
+  let buttonText = event.target.textContent;
+  updateDisplays(buttonText);
 })
 
 // Keyboard Input
