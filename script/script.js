e45895971668729d78e46d6201f8e9df44651354
Ravy Thach <rthach17@gmail.com>
2022-12-24 17:39:36 -0800

    Fix keyboard input with certain keys

--- script/script.js
+++ script/script.js
@@ -2,9 +2,7 @@ const inputWrapper = document.getElementById('input-wrapper');
 inputWrapper.addEventListener("click", event => {
   if (event.target.nodeName == "BUTTON") {
     let button = event.target.textContent;
-    if (button === "DEL") console.log("Backspace");
-    else if (button === "AC") console.log("Clear");
-    else console.log(button);
+    console.log(button);
   }
 })
 
@@ -12,7 +10,8 @@ const inputs = document.querySelectorAll('button');
 document.addEventListener("keydown", event => {
   if(event.repeat) return;
   event.preventDefault();
-  let button = document.getElementById(event.key);
+  let button = document.querySelector(`[class*="${event.key}"]`) || 
+               document.querySelector(`[class*="${event.code}"]`);
   if (button) console.log(button.textContent);
   return;
 });
