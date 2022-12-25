6d5353d759e2bdc91bf1ef44045310597427c8ee
Ravy Thach <rthach17@gmail.com>
2022-12-24 18:00:12 -0800

    Add comments

--- script/script.js
+++ script/script.js
@@ -2,12 +2,14 @@ const inputWrapper = document.getElementById('input-wrapper');
 const inputs = document.querySelectorAll('button');
 const display = document.querySelector('p.display');
 
+// Button Input
 inputWrapper.addEventListener("click", event => {
   if (event.target.nodeName == "BUTTON") {
     display.textContent = event.target.textContent;
     }
 })
 
+// Keyboard Input
 document.addEventListener("keydown", event => {
   if(event.repeat) return;
   event.preventDefault();
